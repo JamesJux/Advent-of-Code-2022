6579ee1c3b6fa75e09dec1d960f598c7cfd3ea38
Dominick Labatz <dominick.labatz@werum.com>
2022-12-09 13:52:03 +0100

    Gestrige Aufgaben..

--- src/com/advent/Main.java
+++ src/com/advent/Main.java
@@ -1,17 +1,20 @@
 package com.advent;
 
+import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+import java.awt.geom.Dimension2D;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 import com.advent.day7.Datei;
 import com.advent.day7.Ordner;
@@ -22,20 +25,22 @@ public class Main {
 
     public static void main(String[] args) {
         String temp = "";
-        // temp += day1("a");
-        // temp += day1("b");
-        // temp += day2("a");
-        // temp += day2("b");
-        // temp += day3("a");
-        // temp += day3("b");
-        // temp += day4("a");
-        // temp += day4("b");
-        // temp += day5("a");
-        // temp += day5("b");
-        // temp += day6("a");
-        // temp += day6("b");
-        // temp += day7("a");
-        temp += day7("b");
+//        temp += day1("a");
+//        temp += day1("b");
+//        temp += day2("a");
+//        temp += day2("b");
+//        temp += day3("a");
+//        temp += day3("b");
+//        temp += day4("a");
+//        temp += day4("b");
+//        temp += day5("a");
+//        temp += day5("b");
+//        temp += day6("a");
+//        temp += day6("b");
+//        temp += day7("a");
+//        temp += day7("b");
+//        temp += day8("a");
+        temp += day8("b");
 
         CopyToClipboard(temp);
         System.out.println(temp);
@@ -80,36 +85,36 @@ public class Main {
                 char first = string.charAt(0);
                 char last = string.charAt(2);
                 switch (first) {
-                case 'A':
-                    if (last == 'Y') {
-                        temp += 8;
-                    } else if (last == 'X') {
-                        temp += 4;
-                    } else {
-                        temp += 3;
-                    }
-                    break;
-                case 'B':
-                    if (last == 'Z') {
-                        temp += 9;
-                    } else if (last == 'Y') {
-                        temp += 5;
-                    } else {
-                        temp += 1;
-                    }
-                    break;
-                case 'C':
-                    if (last == 'X') {
-                        temp += 7;
-                    } else if (last == 'Z') {
-                        temp += 6;
-                    } else {
-                        temp += 2;
-                    }
-                    break;
+                    case 'A':
+                        if (last == 'Y') {
+                            temp += 8;
+                        } else if (last == 'X') {
+                            temp += 4;
+                        } else {
+                            temp += 3;
+                        }
+                        break;
+                    case 'B':
+                        if (last == 'Z') {
+                            temp += 9;
+                        } else if (last == 'Y') {
+                            temp += 5;
+                        } else {
+                            temp += 1;
+                        }
+                        break;
+                    case 'C':
+                        if (last == 'X') {
+                            temp += 7;
+                        } else if (last == 'Z') {
+                            temp += 6;
+                        } else {
+                            temp += 2;
+                        }
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
                 }
                 gesammtPunkte += temp;
             }
@@ -119,35 +124,35 @@ public class Main {
                 char first = string.charAt(0);
                 char last = string.charAt(2);
                 switch (last) {
-                case 'X':
-                    if (first == 'A') {
+                    case 'X':
+                        if (first == 'A') {
+                            temp += 3;
+                        } else if (first == 'B') {
+                            temp += 1;
+                        } else {
+                            temp += 2;
+                        }
+                        break;
+                    case 'Y':
                         temp += 3;
-                    } else if (first == 'B') {
-                        temp += 1;
-                    } else {
-                        temp += 2;
-                    }
-                    break;
-                case 'Y':
-                    temp += 3;
-                    if (first == 'A') {
-                        temp += 1;
-                    } else if (first == 'B') {
-                        temp += 2;
-                    } else {
-                        temp += 3;
-                    }
-                    break;
-                case 'Z':
-                    temp += 6;
-                    if (first == 'A') {
-                        temp += 2;
-                    } else if (first == 'B') {
-                        temp += 3;
-                    } else {
-                        temp += 1;
-                    }
-                    break;
+                        if (first == 'A') {
+                            temp += 1;
+                        } else if (first == 'B') {
+                            temp += 2;
+                        } else {
+                            temp += 3;
+                        }
+                        break;
+                    case 'Z':
+                        temp += 6;
+                        if (first == 'A') {
+                            temp += 2;
+                        } else if (first == 'B') {
+                            temp += 3;
+                        } else {
+                            temp += 1;
+                        }
+                        break;
                 }
                 gesammtPunkte += temp;
             }
@@ -218,8 +223,7 @@ public class Main {
             int elveBsecond = Integer.parseInt(stringArray[1].split("-")[1]);
 
             if (teilaufgabe.equals("a")) {
-                if ((elveAfirst >= elveBfirst && elveAsecond <= elveBsecond)
-                        || (elveBfirst >= elveAfirst && elveBsecond <= elveAsecond)) {
+                if ((elveAfirst >= elveBfirst && elveAsecond <= elveBsecond) || (elveBfirst >= elveAfirst && elveBsecond <= elveAsecond)) {
                     gesammtPunkte += 1;
                 }
             } else {
@@ -313,38 +317,38 @@ public class Main {
         for (String string : stringList) {
             String[] stringArray = string.split(" ");
             switch (stringArray[0]) {
-            case "$": {
-                switch (stringArray[1]) {
-                case "cd": {
-                    if (stringArray[2].equals("..")) {
-                        aktuellerOrdner = aktuellerOrdner.getParent();
-                    } else if (stringArray[2].equals("/")) {
-                        aktuellerOrdner = homeOrdner;
-                    } else {
-                        aktuellerOrdner = aktuellerOrdner.getOrdner(stringArray[2]);
+                case "$": {
+                    switch (stringArray[1]) {
+                        case "cd": {
+                            if (stringArray[2].equals("..")) {
+                                aktuellerOrdner = aktuellerOrdner.getParent();
+                            } else if (stringArray[2].equals("/")) {
+                                aktuellerOrdner = homeOrdner;
+                            } else {
+                                aktuellerOrdner = aktuellerOrdner.getOrdner(stringArray[2]);
+                            }
+                            break;
+                        }
+                        case "ls": {
+                            break;
+                        }
+                        default:
+                            throw new IllegalArgumentException("Unexpected value: " + stringArray[1]);
                     }
                     break;
                 }
-                case "ls": {
+                case "dir": {
+                    aktuellerOrdner.addToOrdnerListe(new Ordner(stringArray[1], aktuellerOrdner));
                     break;
                 }
-                default:
-                    throw new IllegalArgumentException("Unexpected value: " + stringArray[1]);
+                default: {
+                    try {
+                        int groesse = Integer.parseInt(stringArray[0]);
+                        aktuellerOrdner.addToDateiListe(new Datei(stringArray[1], groesse));
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("Unexpected value: " + stringArray[0]);
+                    }
                 }
-                break;
-            }
-            case "dir": {
-                aktuellerOrdner.addToOrdnerListe(new Ordner(stringArray[1], aktuellerOrdner));
-                break;
-            }
-            default: {
-                try {
-                    int groesse = Integer.parseInt(stringArray[0]);
-                    aktuellerOrdner.addToDateiListe(new Datei(stringArray[1], groesse));
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("Unexpected value: " + stringArray[0]);
-                }
-            }
             }
         }
 
@@ -370,6 +374,141 @@ public class Main {
         return antwortAGroesse;
     }
 
+    private static String day8(String teilaufgabe) {
+        file = new File("resources/day8.txt");
+        readFile(file);
+        List<String> stringList = getStringList();
+//        List<String> stringList = List.of("30373", "25512", "65332", "33549", "35390");
+        Dimension2D dim = new Dimension(stringList.size(), stringList.get(0).length());
+        int[][] arrayTree = makeArrayOfStrings(stringList, dim);
+        boolean[][] arrayBool = new boolean[(int) dim.getWidth()][(int) dim.getHeight()];
+        int[][] arraySichtweiten = new int[(int) dim.getWidth()][(int) dim.getHeight()];
+
+        int ergebnis = 0;
+        for (String string : stringList) {
+            if (teilaufgabe.equals("a")) {
+                findHigestTrees(arrayTree, arrayBool, dim, 'X', 0, +1);
+                findHigestTrees(arrayTree, arrayBool, dim, 'X', (int) dim.getWidth() - 1, -1);
+                findHigestTrees(arrayTree, arrayBool, dim, 'Y', 0, +1);
+                findHigestTrees(arrayTree, arrayBool, dim, 'Y', (int) dim.getHeight() - 1, -1);
+                ergebnis = countVisible(arrayBool, dim);
+            } else {
+                arraySichtweiten = zähleSichtweite(arrayTree, arraySichtweiten, dim);
+                ergebnis = sucheMaximaleSichtweite(arraySichtweiten,dim);
+            }
+        }
+        return "" + ergebnis;
+    }
+
+    private static int sucheMaximaleSichtweite(int[][] arraySichtweiten, Dimension2D dim) {
+        int max = 0;
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                if (arraySichtweiten[i][j]>max) {
+                    max =arraySichtweiten[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int[][] zähleSichtweite(int[][] arrayTree, int[][] arraySichtweiten, Dimension2D dim) {
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                int west = getSichtweite(arrayTree, -1, i, j, 0, -1, 0);
+                int sued = getSichtweite(arrayTree, -1, i, j, 1, 0, 0);
+                int ost = getSichtweite(arrayTree, -1, i, j, 0, 1, 0);
+                int nord = getSichtweite(arrayTree, -1, i, j, -1, 0, 0);
+                arraySichtweiten[i][j] = nord * ost * sued * west;
+            }
+        }
+        return arraySichtweiten;
+    }
+
+    private static int getSichtweite(int[][] arrayTree, int startBaumhöhe, int startX, int startY, int x, int y, int sichtweite) {
+        if (startBaumhöhe == -1) {
+            startBaumhöhe = arrayTree[startX][startY];
+        }
+        int baumhöhe;
+        int nextX = startX + x;
+        int nextY = startY + y;
+        try {
+            baumhöhe = arrayTree[nextX][nextY];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return sichtweite;
+        }
+
+        if (startBaumhöhe > baumhöhe) {
+            return sichtweite + 1 + getSichtweite(arrayTree, startBaumhöhe, nextX, nextY, x, y, sichtweite);
+        } else {
+            return sichtweite + 1;
+        }
+    }
+
+    private static int countVisible(boolean[][] arrayBool, Dimension2D dim) {
+        int count = 0;
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                if (arrayBool[i][j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void findHigestTrees(int[][] arrayTree, boolean[][] arrayBool, Dimension2D dim, char achse, int start, int richtung) {
+        if (achse == 'Y') {
+            for (int idx = 0; idx < dim.getWidth(); idx++) {
+                int maxHöhe = -1;
+                for (int j = 0; j < dim.getHeight(); j++) {
+                    if (arrayTree[start + (j * richtung)][idx] > maxHöhe) {
+                        maxHöhe = arrayTree[start + (j * richtung)][idx];
+                        arrayBool[start + (j * richtung)][idx] = true;
+                    }
+                }
+            }
+        } else if (achse == 'X') {
+            for (int idx = 0; idx < dim.getHeight(); idx++) {
+                int maxHöhe = -1;
+                for (int j = 0; j < dim.getWidth(); j++) {
+                    if (arrayTree[idx][start + (j * richtung)] > maxHöhe) {
+                        maxHöhe = arrayTree[idx][start + (j * richtung)];
+                        arrayBool[idx][start + (j * richtung)] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void printArray(int[][] array, Dimension2D dim) {
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printArray(boolean[][] array, Dimension2D dim) {
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] makeArrayOfStrings(List<String> stringList, Dimension2D dim) {
+        int[][] array = new int[(int) dim.getWidth()][(int) dim.getHeight()];
+        for (int i = 0; i < dim.getWidth(); i++) {
+            for (int j = 0; j < dim.getHeight(); j++) {
+                array[i][j] = Integer.parseInt("" + stringList.get(i).charAt(j));
+            }
+        }
+        return array;
+    }
+
     private static int getIndexOfMarker(String string, int anzahl) {
         Set<Character> charSet = new HashSet<>(string.length());
         // System.out.println(string);
@@ -388,7 +527,7 @@ public class Main {
 
     /**
      * Hilfsmethode für Aufgabe Tag 5 Verschiebt chars count oft vom from nach to
-     * 
+     *
      * @param teilaufgabe Die Angabe ob Aufgabenteil 'A' oder 'B' berechnet werden soll.
      * @param stacks      Die Liste aller Stacks
      * @param count       Anzahl der Verschiebungen
@@ -396,8 +535,7 @@ public class Main {
      * @param to          Ziel Stack
      * @return die aktualisierten Stacks , Inplace Ersetzung.
      */
-    private static List<Stack<Character>> moveStackContent(String teilaufgabe, List<Stack<Character>> stacks, int count,
-            int from, int to) {
+    private static List<Stack<Character>> moveStackContent(String teilaufgabe, List<Stack<Character>> stacks, int count, int from, int to) {
         if (teilaufgabe.equals("a")) {
             while (count != 0) {
                 stacks.get(to).push(stacks.get(from).pop());
@@ -419,16 +557,7 @@ public class Main {
     }
 
     private static List<String> getStringList() {
-        List<String> stringList = new ArrayList<String>();
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                stringList.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return stringList;
+        return reader.lines().collect(Collectors.toList());
     }
 
     private static void readFile(File file) {
@@ -441,7 +570,7 @@ public class Main {
 
     /**
      * Kopiert den Text in die Zwischenablage.
-     * 
+     *
      * @param text Den zu kopierenden Text.
      */
     public static void CopyToClipboard(String text) {
